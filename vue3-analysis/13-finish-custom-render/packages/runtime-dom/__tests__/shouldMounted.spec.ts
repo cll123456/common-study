a537d70f2bd1df3e8578f3a01d5bd7a0b1fbcadb
cll123456 <2642137848@qq.com>
2022-06-23 14:44:33 +0800

    feat(13-finish-custom-render): ✨ 添加runtime-dom,runtime-test

---
+++ vue3-analysis/13-finish-custom-render/packages/runtime-dom/__tests__/shouldMounted.spec.ts
@@ -0,0 +1,49 @@
+import { createApp, h, getCurrentInstance } from './../src'
+describe('apiGetCurrentInstance', () => {
+  let appElement: Element;
+  beforeEach(() => {
+    appElement = document.createElement('div');
+    appElement.id = 'app';
+    document.body.appendChild(appElement);
+  })
+
+  afterEach(() => {
+    document.body.innerHTML = '';
+  })
+
+  test('test getCurrentInstance', () => {
+    const Foo = {
+      name: 'Foo',
+      setup() {
+        const instance = getCurrentInstance();
+        expect(instance.type.name).toBe('Foo');
+        return {
+          count: 1
+        }
+      },
+      render() {
+        return h('div', {}, this.count)
+      }
+    }
+
+    const app = createApp({
+      name: 'App',
+      setup() {
+        const instance = getCurrentInstance();
+        expect(instance.type.name).toBe('App');
+        return {
+          count: 2
+        }
+      },
+      render() {
+        return h('div', { class: 'container' }, [h(Foo)])
+      }
+    })
+
+    const appDoc = document.querySelector('#app')
+    app.mount(appDoc);
+
+    const container = document.querySelector('.container') as HTMLElement
+    expect(container.innerHTML).toBe('<div>1</div>')
+  })
+})
