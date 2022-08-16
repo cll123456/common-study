254cabf06dfeea226b73b2b47164a59034b2a3f7
Cll12345 <2642137848@qq.com>
2022-08-16 23:13:48 +0800

    feat: ✨ 88合并两个有序数组

---
+++ leetcodes/88.合并两个有序数组/88.合并两个有序数组.ts
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=88 lang=typescript
+ *
+ * [88] 合并两个有序数组
+ */
+
+// @lc code=start
+/**
+ Do not return anything, modify nums1 in-place instead.
+ */
+function merge(nums1: number[], m: number, nums2: number[], n: number): void {
+  let i = m - 1;
+  let j = n - 1;
+  let k = m + n - 1;
+  while (i >= 0 && j >= 0) {
+    if (nums1[i] > nums2[j]) {
+      nums1[k--] = nums1[i--];
+    } else {
+      nums1[k--] = nums2[j--];
+    }
+  }
+  while (j >= 0) {
+    nums1[k--] = nums2[j--];
+  } 
+};
+// @lc code=end
+
