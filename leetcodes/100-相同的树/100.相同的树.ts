549712d1796a32b2dba4105c6ccbd486a18801ef
Cll12345 <2642137848@qq.com>
2022-08-22 23:35:04 +0800

    feat: ✨ 相同的树

---
+++ leetcodes/100-相同的树/100.相同的树.ts
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=100 lang=typescript
+ *
+ * [100] 相同的树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
+  // 如果不存在
+  if (!p && !q) return true;
+  // 对比两棵树是否相同节点
+  if (!p || !q) return false;
+  if (p.val !== q.val) return false;
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+};
+// @lc code=end
+
