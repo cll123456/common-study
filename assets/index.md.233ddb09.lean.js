8336626fe457c7781d1b3dd5667ac34cf1bd9164
Cll12345 <2642137848@qq.com>
2022-08-26 23:15:45 +0800

    deploy: 自动部署

---
+++ assets/index.md.233ddb09.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as a,a as s}from"./app.d77ad417.js";const u=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"index.md","lastUpdated":1656256654000}'),o={name:"index.md"},n=s("p",null,"'# Hello VitePress'",-1),r=[n];function c(d,i,_,l,p,m){return a(),t("div",null,r)}var x=e(o,[["render",c]]);export{u as __pageData,x as default};
